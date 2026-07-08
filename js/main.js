c1b7dfa0e578a2d6c7324b766639ad40fbccb531
Rido <fridolinkasman@gmail.com>
2026-07-09 02:01:22 +0700

    update kelender

--- js/main.js
+++ js/main.js
@@ -351,7 +351,10 @@ window.addEventListener('load', () => {
     setInterval(loadPengumuman, 600000); // refresh tiap 10 menit
 }
 });
-
+// Setelah fungsi lain dipanggil
+if (typeof refreshKalender === 'function') {
+    refreshKalender();
+}
 // ==================== FIX VIEWPORT MOBILE ====================
 document.addEventListener('gesturestart', function (e) {
     e.preventDefault();
