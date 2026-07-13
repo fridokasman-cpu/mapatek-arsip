98d098e8078c1701e7f7a719c6753ee192393a0c
Rido <fridolinkasman@gmail.com>
2026-07-14 01:22:36 +0700

    up

--- js/data.js
+++ js/data.js
@@ -261,4 +261,5 @@ const searchData = [
     window.quizResults = quizResults;
     window.searchData = searchData;
      window.statsData = statsData; // TAMBAHKAN INI
+     let statsData = {}; // Menambahkan variabel kosong agar tidak error
 })();
