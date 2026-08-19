ebcd6182bee56c0dcef8b8549529e9af489bacb5
Rido <fridolinkasman@gmail.com>
2026-08-20 00:15:20 +0700

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -137,7 +137,7 @@ async function fetchAIResponse(message) {
             },
             body: JSON.stringify({
                 // Kirim body persis seperti yang diharapkan Groq
-              model: "llama3-70b-8192",  // atau model lain yang Anda pakai
+             model: "openai/gpt-oss-120b",  // atau model lain yang Anda pakai
                 messages: [
                     {
                         role: "system",
